4b1b8da6ae3f822a706698120dbaa7e0cc80cc52
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-07-22 15:54:28 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector('.weather');
+const goButton = document.querySelector('.submit-btn');
+const displayWeather = document.querySelector('.display-weather');
+
+weather.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const city = cityField.value;
+
+  async function getWeatherData() {
+    const apiKey = '919012c8a99a4419a6821434232207';
+
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+      );
+      const data = await response.json();
+
+      if (response.status === 200) {
+        const weatherInfo = {
+          city: data.location.name,
+          state: data.location.region,
+          description: data.current.condition.text,
+          temperature: data.current.temp_f,
+          humidity: data.current.humidity,
+          icon: data.current.condition.icon,
+        };
+
+        displayWeatherInfo(weatherInfo);
+      } else {
+        console.log('Weather API request failed:', data.message);
+      }
+    } catch (error) {
+      console.log('Error fetching weather data:', error);
+    }
+  }
+
+  function displayWeatherInfo(weatherInfo) {
+    const weatherDiv = document.querySelector('.display-weather');
+
+    displayWeather.style.marginTop = '25px';
+
+    weatherDiv.innerHTML = `<h2 class="display-header">${weatherInfo.city}, ${weatherInfo.state}</h2>
+        <div class="description-icon">
+          <div class="description">
+            <p></p>
+            <p>${weatherInfo.description}</p>
+            <p>Temp: ${weatherInfo.temperature}</p>
+            <p>Humidity: ${weatherInfo.humidity} %</p>
+          </div>
+          <div class="icon">
+            <img src="${weatherInfo.icon}" alt="sadf" />
+          </div>
+        </div>`;
+  }
+  getWeatherData();
+});
